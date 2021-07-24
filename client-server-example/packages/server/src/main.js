d02b3194e8ab911ffbca8bdea37c80deb3b65980
Fabio Haddad <fabiohpaes@gmail.com>
2021-07-24 00:16:53 -0300

    [GraphQL] Types and Resolvers

--- client-server-example/packages/server/src/main.js
+++ client-server-example/packages/server/src/main.js
@@ -1,28 +1,15 @@
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
+
 const app = express();
+
 async function startApolloServer() {
     const server = new ApolloServer({
-        typeDefs: gql`
-            type Client {
-                id: ID!
-                name: String!
-            }
-            type Demand {
-                id: ID!
-                name: String!
-                client: Client!
-                deadline: String
-            }
-            type Query {
-                demands: [Demand]!
-            }
-        `,
-        resolvers: {
-            Query: {
-                demands: () => [],
-            },
-        }
+        typeDefs,
+        resolvers,
     });
     await server.start();
     server.applyMiddleware({
@@ -30,28 +17,12 @@ async function startApolloServer() {
         cors: {
             origin: 'http://localhost:3000'
         },
+		bodyParserConfig: true,
     });
-    //server.get('/status', (_, response) => {
-    //    response.send({
-    //        status: 'Okay',
-    //    });
-    //});
-    // server
-    //     .options('/authenticate', enableCors)
-    //     .post('/authenticate', enableCors, express.json(), (request, response) => {
-    //         console.log(
-    //             'E-mail', request.body.email,
-    //             'Senha', request.body.senha
-    //         );
-    //         response.send({
-    //             Okay: true
-    //         });
-    //     });
+
     const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
     const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
-    // await server.listen(PORT, HOSTNAME, () => {
-    //     console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
-    // })
+
     await new Promise(resolve => app.listen({ port: PORT }, resolve));
     return { server, app };
 }
