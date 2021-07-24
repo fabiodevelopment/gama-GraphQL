f1dfa596e512007203fc2f4aa8938e2da4c7c49c
Fabio Haddad <fabiohpaes@gmail.com>
2021-07-23 23:43:50 -0300

    [GraphQL] Install and configure apolo-server-express 2.13.1

--- client-server-example/packages/server/src/main.js
+++ client-server-example/packages/server/src/main.js
@@ -1,34 +1,58 @@
 import express from 'express';
-import cors from 'cors';
-
-const server = express();
-
-// server.use(cors()); // Habilita o tratamento do cors para toda requisição
-
-server.get('/status', (_, response) => {
-	response.send({
-		"status": "Okay",
-	});
-});
-
-const enableCors = cors({ origin: 'http://localhost:3000'});
-
-server
-.options('/authenticate', enableCors)
-.post('/authenticate', enableCors, express.json(), (request, response) => {
-	console.log(
-		'E-mail', request.body.email,
-		'Senha', request.body.password, 
-	);
-	response.send({ 
-		Okay: true,
-	});
-});
-
-
-const PORT = process.env.PORT ? parseInt(process.env.PORT) :8000;
-const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
-
-server.listen( PORT, HOSTNAME, () =>{
-	console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
-})
+import { ApolloServer, gql } from 'apollo-server-express';
+const app = express();
+async function startApolloServer() {
+    const server = new ApolloServer({
+        typeDefs: gql`
+            type Client {
+                id: ID!
+                name: String!
+            }
+            type Demand {
+                id: ID!
+                name: String!
+                client: Client!
+                deadline: String
+            }
+            type Query {
+                demands: [Demand]!
+            }
+        `,
+        resolvers: {
+            Query: {
+                demands: () => [],
+            },
+        }
+    });
+    await server.start();
+    server.applyMiddleware({
+        app,
+        cors: {
+            origin: 'http://localhost:3000'
+        },
+    });
+    //server.get('/status', (_, response) => {
+    //    response.send({
+    //        status: 'Okay',
+    //    });
+    //});
+    // server
+    //     .options('/authenticate', enableCors)
+    //     .post('/authenticate', enableCors, express.json(), (request, response) => {
+    //         console.log(
+    //             'E-mail', request.body.email,
+    //             'Senha', request.body.senha
+    //         );
+    //         response.send({
+    //             Okay: true
+    //         });
+    //     });
+    const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+    const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+    // await server.listen(PORT, HOSTNAME, () => {
+    //     console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
+    // })
+    await new Promise(resolve => app.listen({ port: PORT }, resolve));
+    return { server, app };
+}
+startApolloServer();
